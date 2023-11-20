c492169101f50c01f6559d7de4e2c18c2532033c
thunderdoge <thunderdoge@qq.com>
2023-11-20 10:11:42 +0800

    visual. rearrange & LQR controller.

--- aerial_robot/common/posRotVisualize.m
+++ aerial_robot/common/posRotVisualize.m
@@ -1,30 +1,37 @@
 function posRotVisualize(Pdata,Rdata,tdata,interval)
 % 'out' is Simulink output variable.
 %% Prepare Source Data
-P = Pdata;
+P = squeeze(Pdata);
 [d1,d2] = size(P);
 if d1 > d2
     P = transpose(P);
 end
 n = length(tdata);
 R = zeros(size(Rdata));
+
+zaxis_down = false;
+if zaxis_down
 for i = 1:n
     R(:,:,i) =-1 * eye(3) * Rdata(:,:,i);
 end
+else
+    R = Rdata;
+end
 frameselect = 1:interval:n;
 t = squeeze(tdata(frameselect));
+nselect = length(frameselect);
 
 %% Graph Data & Config
-set(gcf,'PaperPositionMode','auto');
+% set(gcf,'PaperPositionMode','auto');
 [x,y,z,u,v,w] = genQuiver3Data(P(:,frameselect),R(:,:,frameselect));
-
-q = quiver3(0,0,0,0,0,0);
+zv = zeros(3,1); % z3ro vector.
+q = quiver3(zv,zv,zv,zv,zv,zv);
 q.Marker = '.';
 q.MaxHeadSize = 0.5;
 q.AutoScale = true;
 q.AutoScaleFactor = 1.0;
 axis vis3d;
-axis([-10, 10, -10, 10, -2, 20]);
+axis([-1, 3, -1, 3, -1, 3]);
 
 % q.XDataSource = 'xi';
 % q.YDataSource = 'yi';
@@ -40,7 +47,7 @@ axis([-10, 10, -10, 10, -2, 20]);
 % q.WDataMode = "auto";
 
 %% Gen Movie (by edit graph and gen frame)
-for i=1:n
+for i=1:nselect
     disp(i)
     xi = repmat(x(i),[3, 1]);
     yi = repmat(y(i),[3, 1]);
